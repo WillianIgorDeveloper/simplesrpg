bf2218aea9e37e28bfb69b87ea0f598a0225294e
Willian Igor <willianigordeveloper@gmail.com>
2023-09-07 09:20:47 -0300

    update: finishing home component

--- src/contexts/Supabase.tsx
+++ src/contexts/Supabase.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
 type SupabaseContextType = {};
 
@@ -7,9 +7,9 @@ type SupabaseProviderType = {
 	children: React.ReactNode;
 };
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const SupabaseContext = createContext({} as SupabaseContextType);
 export const SupabaseProvider = ({ children }: SupabaseProviderType) => {
