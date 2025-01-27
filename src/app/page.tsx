9b9d3ade255583a8afc995ebcfbcc24ccb1eabe7
Willian Igor <willanigordeveloper@gmail.com>
2025-01-27 10:11:19 -0300

    add a component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+import { TopBar } from "@components/top-bar"
+import { Button } from "@heroui/react"
+
 export default function Page() {
   return (
     <div>
+      <TopBar />
       <h1>Simples RPG</h1>
+      <Button color="primary">Start</Button>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss"
+import { heroui } from "@heroui/react"
 
 export default {
-  content: ["./src/presenters/**/*.tsx"],
+  darkMode: "class",
+  plugins: [heroui()],
+  content: [
+    "./src/presenters/**/*.tsx",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {},
-  plugins: [],
 } satisfies Config
